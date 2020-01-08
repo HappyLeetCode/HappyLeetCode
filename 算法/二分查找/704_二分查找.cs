e702170489240db92d459d5be76981a832e2d1c1
蔡遵造 <caizunzao@4399inc.com>
2020-01-08 17:16:15 +0800

    去除不需要的注释

--- 算法/二分查找/704_二分查找.cs
+++ 算法/二分查找/704_二分查找.cs
@@ -7,7 +7,7 @@ public class Solution
     public int Search(int[] nums, int target)
     {
         int left = 0;
-        int right = nums.Length - 1; // 注意
+        int right = nums.Length - 1;
 
         while (left <= right)
         {
@@ -15,9 +15,9 @@ public class Solution
             if (nums[mid] == target)
                 return mid;
             else if (nums[mid] < target)
-                left = mid + 1; // 注意
+                left = mid + 1;
             else if (nums[mid] > target)
-                right = mid - 1; // 注意
+                right = mid - 1;
         }
         return -1;
     }
