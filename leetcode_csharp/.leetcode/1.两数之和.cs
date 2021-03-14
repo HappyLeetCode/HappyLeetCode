cb2046a6e87c8b1acd80fc52eb37d1e846cc4f4d
caizunzao <csoap994@gmail.com>
2021-03-15 00:29:59 +0800

    动态规划 不同路径

--- leetcode_csharp/.leetcode/1.两数之和.cs
+++ leetcode_csharp/.leetcode/1.两数之和.cs
@@ -10,11 +10,9 @@ public class Solution {
         Dictionary<int, int> dic = new Dictionary<int, int>();
         for (int i = 0; i < nums.Length; i++)
         {
-            if (dic.ContainsKey(target - nums[i]))
-            {
+            if(dic.ContainsKey(target - nums[i])){
                 return new int[] {i, dic[target - nums[i]]};
-            }else
-            {
+            }else{
                 dic.Add(nums[i], i);
             }
         }
---
+++ leetcode_csharp/.leetcode/50.pow-x-n.cs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=50 lang=csharp
+ *
+ * [50] Pow(x, n)
+ */
+
+// @lc code=start
+public class Solution {
+    public double MyPow(double x, int n) {
+
+    }
+}
+// @lc code=end
+
